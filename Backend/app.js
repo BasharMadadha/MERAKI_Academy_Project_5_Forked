38cc158fb83fa9e69132f350b6e8cbbcb172c266
MaenAlNjar <maen.njarr@gmail.com>
2023-09-20 23:57:11 +0300

    user

--- Backend/app.js
+++ Backend/app.js
@@ -3,15 +3,16 @@ const app =express()
 require('dotenv').config(); 
 const PORT =process.env.PORT || 5000
 
-const pool = require('./db');
+const pool = require('./models/db');
 
 app.use(express.json());
 
 
+const userRouter = require("./routes/User");
 
+app.use("/",userRouter)
 
-
-
+app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
 app.listen(PORT,()=>{
     console.log(`server is running on post=${PORT}`);
