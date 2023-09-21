58da0fc2e1c3ff8fe246f31ac35dae94c3e0e786
BasharMadadha <Bashar.Madadhaa@gmail.com>
2023-09-22 00:09:52 +0300

    posts

--- Backend/controller/posts.js
+++ Backend/controller/posts.js
@@ -1,10 +1,10 @@
-const {pool} = require("../models/db");
+const { pool } = require("../models/db");
 
 const createNewPost = (req, res) => {
-  const { title, body } = req.body;
+  const { title, content, image_url } = req.body;
   const user_id = req.token.userId;
-  const query = `INSERT INTO posts (title, body, user_id) VALUES ($1,$2,$3) RETURNING *;`;
-  const data = [title, body, user_id];
+  const query = `INSERT INTO posts (title, content, user_id,image_url) VALUES ($1,$2,$3,$4) RETURNING *;`;
+  const data = [title, content, user_id, image_url];
   pool
     .query(query, data)
     .then((result) => {
@@ -18,7 +18,7 @@ const createNewPost = (req, res) => {
       res.status(500).json({
         success: false,
         message: "Server error",
-        err: err,
+        err: err.message,
       });
     });
 };
@@ -39,13 +39,13 @@ const getAllPosts = (req, res) => {
       res.status(500).json({
         success: false,
         message: "Server error",
-        err: err,
+        err: err.message,
       });
     });
 };
 
 const getPostsByUser = (req, res) => {
-  const user_id = req.query.user;
+  const user_id = req.params.user_id;
   const query = `SELECT * FROM posts WHERE user_id = $1 AND is_deleted=0;`;
   const data = [user_id];
 
@@ -69,14 +69,21 @@ const getPostsByUser = (req, res) => {
       res.status(500).json({
         success: false,
         message: "Server error",
-        err: err,
+        err: err.message,
       });
     });
 };
 
 const getPostById = (req, res) => {
   const id = req.params.id;
-  const query = `SELECT title,body,user_id FROM users INNER JOIN posts ON users.id=posts.user_id WHERE posts.id=$1 AND posts.is_deleted=0;`;
+  const query = `SELECT
+  posts.title,
+  posts.content,
+  posts.user_id,
+  posts.image_url
+  FROM posts
+  INNER JOIN users ON posts.user_id = users.id
+  WHERE posts.post_id = $1 AND posts.is_deleted = 0;`;
   const data = [id];
 
   pool
@@ -96,24 +103,24 @@ const getPostById = (req, res) => {
       res.status(500).json({
         success: false,
         message: "Server error",
-        err: err,
+        err: err.message,
       });
     });
 };
 
 const updatePostById = (req, res) => {
-  const id = req.params.id;
-  let { title, body } = req.body;
+  const post_id = req.params.id;
+  let { title, content } = req.body;
 
-  const query = `UPDATE posts SET title = COALESCE($1,title), body = COALESCE($2, body) WHERE id=$3 AND is_deleted = 0  RETURNING *;`;
-  const data = [title || null, body || null, id];
+  const query = `UPDATE posts SET title = COALESCE($1,title), content = COALESCE($2, content) WHERE post_id=$3 AND is_deleted = 0  RETURNING *;`;
+  const data = [title || null, content || null, post_id];
   pool
     .query(query, data)
     .then((result) => {
       if (result.rows.length !== 0) {
         res.status(200).json({
           success: true,
-          message: `post with id: ${id} updated successfully `,
+          message: `post with id: ${post_id} updated successfully `,
           result: result.rows[0],
         });
       } else {
@@ -124,23 +131,23 @@ const updatePostById = (req, res) => {
       res.status(500).json({
         success: false,
         message: "Server error",
-        err: err,
+        err: err.message,
       });
     });
 };
 //
 
 const deletePostById = (req, res) => {
-  const id = req.params.id;
-  const query = `UPDATE posts SET is_deleted=1 WHERE id=$1;`;
-  const data = [id];
+  const post_id = req.params.id;
+  const query = `UPDATE posts SET is_deleted=1 WHERE post_id=$1;`;
+  const data = [post_id];
   pool
     .query(query, data)
     .then((result) => {
       if (result.rowCount !== 0) {
         res.status(200).json({
           success: true,
-          message: `posts with id: ${id} deleted successfully`,
+          message: `posts with id: ${post_id} deleted successfully`,
         });
       } else {
         throw new Error("Error happened while deleting post");
@@ -150,7 +157,7 @@ const deletePostById = (req, res) => {
       res.status(500).json({
         success: false,
         message: "Server error",
-        err: err,
+        err: err.message,
       });
     });
 };
@@ -175,7 +182,7 @@ const deletePostByUser = (req, res) => {
       res.status(500).json({
         success: false,
         message: "Server error",
-        err: err,
+        err: err.message,
       });
     });
 };
